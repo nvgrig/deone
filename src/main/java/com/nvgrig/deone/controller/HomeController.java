cdc0cad00861db67574cf2c84e3228216ae86708
Nikita Grigorev <45516608+nvgrig@users.noreply.github.com>
2024-04-03 14:57:43 +0400

    controller refactoring and adding a simple form

--- src/main/java/com/nvgrig/deone/controller/HomeController.java
+++ src/main/java/com/nvgrig/deone/controller/HomeController.java
@@ -1,11 +1,15 @@
 package com.nvgrig.deone.controller;
 
+import com.nvgrig.deone.model.Book;
 import com.nvgrig.deone.repository.BookRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
 
     private BookRepository bookRepository;
@@ -14,9 +18,18 @@ public class HomeController {
         this.bookRepository = bookRepository;
     }
 
-    @RequestMapping("/")
+    @GetMapping
     public String getIndex(Model model) {
         model.addAttribute("books", bookRepository.findAll());
+        model.addAttribute("newBook", new Book());
+
         return "index";
     }
+
+    @PostMapping
+    public String createBook(Book book) {
+        bookRepository.save(book);
+        return "redirect:/";
+    }
+
 }
